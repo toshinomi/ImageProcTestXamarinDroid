9ee26cca011994014c4de21e9a2b67f974238391
toshinomi <tatsuya.oshinomi.0828@gmail.com>
2020-05-23 23:53:31 +0900

    プロジェクト ファイルを追加します。

---
+++ GrayScale.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Graphics;
+
+class GrayScale
+{
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public GrayScale()
+    {
+    }
+
+    /// <summary>
+    /// デスクトラクタ
+    /// </summary>
+    ~GrayScale()
+    {
+    }
+
+    /// <summary>
+    /// グレースケールの実行
+    /// </summary>
+    /// <param name="image">ビットマップ</param>
+    /// <returns>グレースケール後のビットマップ</returns>
+    public Bitmap GoImgProc(Bitmap bitmap)
+    {
+        var pixels = new int[bitmap.Width * bitmap.Height];
+        var resultPixels = new int[bitmap.Width * bitmap.Height];
+
+        bitmap.GetPixels(pixels, 0, bitmap.Width, 0, 0, bitmap.Width, bitmap.Height);
+
+        int count = 0;
+        foreach (var argb in pixels)
+        {
+            var color = new Android.Graphics.Color(argb);
+            var gray = (byte)((color.R + color.G + color.B) / 3);
+            resultPixels[count++] = Android.Graphics.Color.Argb(color.A, gray, gray, gray);
+        }
+
+        var mutableBitmap = Bitmap.CreateBitmap(bitmap.Width, bitmap.Height, bitmap.GetConfig());
+        mutableBitmap.SetPixels(resultPixels, 0, mutableBitmap.Width, 0, 0, mutableBitmap.Width, mutableBitmap.Height);
+
+        return mutableBitmap;
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,73 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Android.Graphics;
+using System.Threading.Tasks;
+
+namespace ImageProcTestXamarinDroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private ImageView   mImageView;
+        private Bitmap      mBitmap;
+        private Button      mBtnMono;
+        private Button      mBtnReset;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            InitLayout();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        /// <summary>
+        /// レイアウトの初期設定
+        /// </summary>
+        public void InitLayout()
+        {
+            mImageView = (ImageView)FindViewById(Resource.Id.image);
+
+            mBitmap = BitmapFactory.DecodeResource(Resources, Resource.Drawable.dog);
+
+            mBtnMono = (Button)FindViewById(Resource.Id.mono);
+            mBtnMono.Click += OnClickBtnMono;
+
+            mBtnReset = (Button)FindViewById(Resource.Id.reset);
+            mBtnReset.Click += OnClickBtnReset;
+        }
+
+        /// <summary>
+        /// モノクロボタンのクリックイベント
+        /// </summary>
+        /// <param name="s">オブジェクト</param>
+        /// <param name="e">イベントのデータ</param>
+        private async void OnClickBtnMono(object s, EventArgs e)
+        {
+            var gray = new GrayScale();
+            var mutableBitmap = await Task.Run(() => gray.GoImgProc(mBitmap));
+            mImageView.SetImageBitmap(mutableBitmap?.Copy(Bitmap.Config.RgbaF16, false));
+        }
+
+        /// <summary>
+        /// リセットボタンのクリックイベント
+        /// </summary>
+        /// <param name="s">オブジェクト</param>
+        /// <param name="e">イベントのデータ</param>
+        private void OnClickBtnReset(object s, EventArgs e)
+        {
+            mImageView.SetImageBitmap(mBitmap);
+        }
+    }
+}
